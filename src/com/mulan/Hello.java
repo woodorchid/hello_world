19501bb3f74c7811590b6eca3dcd6072c42eb03d
Mulan-hzx <hzx68526@163.com>
2021-04-11 20:00:37 +0800

    第一次提交

---
+++ src/com/mulan/Hello.java
@@ -0,0 +1,12 @@
+package com.mulan;
+
+/**
+ * @author 韩志雄
+ * @date 2021/4/11 19:58
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
